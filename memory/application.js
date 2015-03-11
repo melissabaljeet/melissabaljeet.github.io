ae6719d7e9c8b79fb63c6c30623c4c5d48e7d42f
melissabaljeet <mbaljeet@kippnyccp.org>
2015-03-11 09:12:08 -0400

    Create application.js

---
+++ memory/application.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var click_count = 0;
+  var myarray = ["1","1","2","2","3","3"];
+  for(i in myarray){
+    $('#card_holder').append('<div class="card"><p>'+ myarray[i]+'</p></div>');
+  }                                       
+  //$(this).css("border","1px");
+    $('.card').on("click", function(){
+        if (click_count == 0) {
+        $(this).find('p').css('opacity', 1);
+        }
+    $('.card').on("click", function(){
+      if (click_count== 1){
+       $(this).find('p').css('opacity', 1);
+      }
+    });
+
+});
