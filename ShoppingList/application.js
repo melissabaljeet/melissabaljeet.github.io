64252e281c1972f624adddf2b439a6f90afc95d1
melissabaljeet <mbaljeet@kippnyccp.org>
2015-03-19 08:52:59 -0400

    Update application.js

--- ShoppingList/application.js
+++ ShoppingList/application.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+   $('button').on("click", function(){
+    $('input').val();
+    var value= $('input').val();
+    $('#1').append("<p>"+value+"</p>");
+   })
+}
+)
