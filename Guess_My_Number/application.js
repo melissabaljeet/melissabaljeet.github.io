d41a64027e920e9d7531cc29a44df4c6f4cbae42
melissabaljeet <mbaljeet@kippnyccp.org>
2015-03-30 08:52:34 -0400

    Create application.js

---
+++ Guess_My_Number/application.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  //find a way for the program to choose a random number between 1 and 100, save this as a variable
+  
+  
+  //when the player clicks on the 'guess' button
+  $('button').on('click', function(){
+    //save their guess as a variable
+    
+    
+    //and compare this guess to the random number that the computer picked
+    //if the user guessed the correct number...
+    
+      //what happens if the guess is correct?
+      
+    //if the user guessed too high...
+    
+      //update the 'feedback' paragraph to tell them to guess lower
+      
+    //otherwise, the user guessed too low...
+    
+      //update the 'feedback' paragraph to tell them to guess higher
+    
+  });
+});
