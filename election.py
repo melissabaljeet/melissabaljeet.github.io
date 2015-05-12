13952669986477a30fd723010c3b1639396e780a
melissabaljeet <mbaljeet@kippnyccp.org>
2015-05-12 09:02:39 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,6 +1,15 @@
-Candidate_1=()
-Candidate_2=()
-wins= (Candidate_1, Candidate_2)
-number_of_wins = random()*10 
-if wins = Candidate_1
-  add 
+from random import randint
+Candidate_1=0
+
+for i in range(10000):
+  regions= (0) #Checks to see the wins based on percentage with each region
+  if randint(1,100) <= 87:
+    regions += 1
+  if randint(1,100) <= 65:
+    regions += 1
+  if randit(1,100) <= 17:
+    regions += 1
+  if regions >= 2:
+    Candidate_1 += 1
+
+print "The chances of winning are" + "Candidate_1/ 10000"
