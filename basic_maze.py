6d407f0defaf39968276e7496901217bd67c6bab
melissabaljeet <mbaljeet@kippnyccp.org>
2015-05-19 09:02:16 -0400

    Create basic_maze.py

---
+++ basic_maze.py
@@ -0,0 +1,14 @@
+from gopigo import
+import time
+import math
+
+set_speed(75)
+
+enc_tgt(1,1,18)
+time.sleep(.1)
+fwd()
+time.sleep(3)
+
+def move_forward(feet):
+  1 foot = 304.8 mm
+  motor_steps =11.34 mm 
