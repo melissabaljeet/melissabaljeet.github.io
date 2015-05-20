51b404ac5ba09923b8b7f846368c0de37d3dcb44
melissabaljeet <mbaljeet@kippnyccp.org>
2015-05-20 09:01:08 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -1,15 +1,27 @@
-from gopigo import
+from GoPiGo import
 import time
 import math
-
 set_speed(75)
 
-enc_tgt(1,1,18)
-time.sleep(.1)
-fwd()
-time.sleep(3)
 
 def move_forward(feet):
-  1 foot = 304.8 mm
-  motor_steps =11.34 mm 
-  move_forward
+  mm = feet * 304.8
+  motor_steps = int(mm / 11.34)
+  enc_tgt(1,1,motor_steps)
+  time.sleep(.1)
+  fwd()
+  time.sleep(feet*2)
+
+
+def turn_left:
+  enc_tgt(1,0, 2)
+  time.sleep(.1)
+  fwd()
+  time.sleep(5)
+  
+  
+def turn_right:
+  enc_tgt(0,1,2)
+  time.sleep(.1)
+  fwd()
+  time.sleep(5)
