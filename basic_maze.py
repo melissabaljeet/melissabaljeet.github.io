63826b8cc372339115a2f3f6412d5c8a0e4fefe8
melissabaljeet <mbaljeet@kippnyccp.org>
2015-05-21 08:42:08 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -13,15 +13,21 @@ def move_forward(feet):
   time.sleep(feet*2)
 
 
-def turn_left:
+def turn_left():
   enc_tgt(1,0, 2)
   time.sleep(.1)
   fwd()
   time.sleep(5)
   
   
-def turn_right:
+def turn_right():
   enc_tgt(0,1,2)
   time.sleep(.1)
   fwd()
   time.sleep(5)
+
+move_forward(4)
+turn_right
+move_forward(2)
+turn_right
+move_forward(4)
